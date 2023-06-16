984568301ee3fb18dc7ebe4a213d42994d63faae
ck <none>
2023-06-16 16:51:52 +0200

    changed call_gpt to call_chat

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ if __name__ =='__main__':
     @bot.command()
     async def chat_gpt(ctx, *args):
         texte = ' '.join(args)
-        response = gpt.call_gpt(texte)
+        response = gpt.call_chat(texte)
         await bot.send_message(ctx, response ,False)
+
+
 
     bot.run(os.getenv("discord_token"))
--- src/_chatGpt.py
+++ src/_chatGpt.py
@@ -5,6 +5,7 @@ class Gpt():
         openai.api_key = api_key
 
 
-    def call_gpt(self, content):
+    def call_chat(self, content):
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": content}])
         return chat_completion.choices[0].message.content 
+    
