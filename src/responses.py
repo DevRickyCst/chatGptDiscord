bcc44b80344a675c829876562fe6e86ba0a756db
ck <none>
2023-06-16 03:15:38 +0200

    test

--- src/responses.py
+++ src/responses.py
@@ -3,7 +3,7 @@ import os
 
 def handle_response(message):
     gpt = Gpt(os.getenv("openai_api_key"))
-    print('handle respons')
+    print('handle response')
 
     print(message)
     return(gpt.call_gpt(message))
