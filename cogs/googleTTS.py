26b71f95c2636b6c1e048116e3b8f813b1d478f7
Rickycst <creusot.aymeric7@gmail.com>
2023-06-27 19:12:15 +0200

    Update way of doing use cogs

---
+++ cogs/googleTTS.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+from src._googleTTS import gTTS
+
+class openai(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def chat_gpt(self, ctx, *args):
+        channel = ctx.author.voice.channel if ctx.author.voice!=None else None
+        message = ' '.join(args) if len(args)>=1 else 'SMAB'
+        if channel!=None and ctx.voice_client==None:
+            await self.bot.play_audio(gTTS.get_audio_fp(message), channel)
+            
--- main.py
+++ main.py
@@ -1,40 +1,37 @@
-from src._botDiscord import BotDiscord
+from cogs.src._botDiscord import BotDiscord
 import os
 from dotenv import load_dotenv
-from src._googleTTS import GoogleTTS as gTTS
-from src._chatGpt import Gpt
+from cogs.src._googleTTS import GoogleTTS as gTTS
+from cogs.src._chatGpt import Gpt
 
 load_dotenv()
+
+cogs =  [
+    'openai',
+    'googleTTS'
+]
+
+
+async def load_env():
+    for cog in cogs:
+        try:
+            await bot.load_extension(cog)
+        except:
+            f"Couldn't load {cog} module"
+
+
 if __name__ =='__main__':
 
     bot = BotDiscord()
     gpt = Gpt(os.getenv("openai_api_key"))
 
 
-    @bot.command()
-    async def chat_gpt(ctx, *args):
-        texte = ' '.join(args)
-        response = gpt.call_chat(texte)
-        await bot.send_message(ctx, response ,False)
+    @bot.event
+    async def on_ready():
+        print(f'Connecté en tant que {bot.user.name} ({bot.user.id})')
+        print('------')
 
-    @bot.command()
-    async def img_gpt(ctx, *args):
-        texte = ' '.join(args)
-        response = gpt.call_image(texte)
-        await bot.send_message(ctx, response ,False)
-    
-    @bot.command()
-    async def trad_gpt(ctx, *args):
-        lang = args[0]
-        texte = ' '.join(args[1:])
-        response = gpt.call_traduction(lang,texte)
-        await bot.send_message(ctx, response ,False)
+        await load_env()
 
-    @bot.command()
-    async def parle(ctx, *args):
-        channel = ctx.author.voice.channel if ctx.author.voice!=None else None
-        message = ' '.join(args) if len(args)>=1 else 'SMAB'
-        if channel!=None and ctx.voice_client==None:
-            await bot.play_audio(gTTS.get_audio_fp(message), channel)
 
     bot.run(os.getenv("discord_token"))
